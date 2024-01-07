3ed0c312cc7271624e18ccf6da5332da1a945481
srodenas <srodher115@g.educaand.es>
2024-01-07 16:29:43 +0100

    incluyo el botton como include

--- app/src/main/java/com/santi/pmdm/virgen/hospedaje/MainActivity.kt
+++ app/src/main/java/com/santi/pmdm/virgen/hospedaje/MainActivity.kt
@@ -43,9 +43,6 @@ import com.santi.pmdm.virgen.hospedaje.fragment.subscriptions.SubscriptionsFragm
 class MainActivity : AppCompatActivity(){
     lateinit var binding : ActivityMainBinding
 
-
-
-
     /*
     Un NavigationView, maneja el menú del Drawer y el header.
      */
@@ -55,9 +52,7 @@ class MainActivity : AppCompatActivity(){
         binding =ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
         initFab()
-
         initProcessDrawer()
 
 
@@ -68,23 +63,57 @@ class MainActivity : AppCompatActivity(){
      */
     private fun initProcessDrawer() {
 
-       // val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment //Nuestro NavHostFragment
-       // val navController = navHostFragment.navController //Nuestro navController
+        /*
+        Esta forma sería válida, si en vez de un container fuera un fragment
+
+         val navController = findNavController(R.id.container_fragment)
+         */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment //Nuestro NavHostFragment
+        val navController = navHostFragment.navController //Nuestro navController
+
+        /*
+        1.- Personalizamos nuestra barra de superior personalizada. La incorporamos.
+        2.- Hay que asegurarnos, de que el style deba extender de un tema que incluya un ActionBar.
+        Nosotros hemos utilizado Theme.MaterialComponents.DayNight.DarkActionBar
+         */
         setSupportActionBar(binding.appBarMain.myToolbar)
 
-        val navController = findNavController(R.id.container_fragment)
+
+        //Necesitamos nuestro componente de navegación.
         val navView = binding.myNavView
+
+        /*
+        Con appBarConfiguration, nos aseguramos la CONFIGURACIÓN mediante un drawer y sus destinos.
+        1.- Administra el botón de navegación. Consideramos primer nivel todos excepto el listado de hoteles. Por tanto, configura destinos.
+        2.- Como tenemos un DrawerLayout, lo configuramos para que el icono cambie con el icono de la hamburguesa cuando esté en el home y con la
+        flecha <- cuando esté en un nivel inferior.
+        3.- Hacemos que se sincronice o funcione con el navigation drawer.
+         */
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.homeFragment,R.id.settingsFragment, R.id.shareFragment, R.id.aboutFragment,
                      R.id.shortsFragment, R.id.subscriptionsFragment, R.id.libraryFragment),
             binding.myDrawer
         )
-        setupActionBarWithNavController(navController, appBarConfiguration) //Configuro la barra de acciones con el NavController y appBarConfiguration
+
+
+        /*
+        1.- Aplica la configuración del appBarConfiguration, vinculándolo con la navegación a partir del navController.
+        2.- !!!!!Sin esto, no veríamos el botón de navegación !!!!!!
+         */
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        /*
+        Hacemos que el componente de navegación, funcione correctamente con cada uno de los elementos del Drawer.
+        1.- Sin esto, no podrá navegar a ningún destino.
+         */
         navView.setupWithNavController(navController)
 
-
-       // binding.myNavigationDrawer.setupWithNavController(navController) //Configuro la navegación para el Drawer con el navController.
-        binding.appBarMain.myBottonNavigation.setupWithNavController(navController)
+        /*
+        Hacemos que el componente de navegación, funcione correctamente con cada uno de los elementos del Bottom
+        1.- Sin esto, no podrá navegar a ningún destino.
+         */
+        binding.appBarMain.appBarMain.myBottonNavigation.setupWithNavController(navController)
 
     }
 
@@ -94,6 +123,14 @@ class MainActivity : AppCompatActivity(){
         return true
     }
 
+
+
+    /*
+    Con este método, hacemos que funcione correctamente el botón de navegación hacia arriba.
+    1.- Esto hace que que responda a los eventos de navegación. Controlará la apertura de abrir
+    la barra lateral del drawer y su cierre.
+    2.- Sin esto, no se abre/cierra el Drawer.
+     */
     override fun onSupportNavigateUp(): Boolean{
 
         val navController = findNavController(R.id.container_fragment)
@@ -101,12 +138,9 @@ class MainActivity : AppCompatActivity(){
 
 
     private fun initFab(){
-        binding.appBarMain.fab.setOnClickListener{
+        binding.appBarMain.appBarMain.fab.setOnClickListener{
             Toast.makeText(this, "Pulso + ", Toast.LENGTH_LONG).show()
         }
     }
-
-
-
 
 }
